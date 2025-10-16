ec53186d0d1016954bf4dbe92cbf0376e21942a4
irfan-mahbub <163571611+irfan-mahbub@users.noreply.github.com>
2025-10-16 17:51:04 +0600

    Add files via upload

---
+++ TC1.java
@@ -0,0 +1,11 @@
+// Widening Casting
+
+public class TC1 {
+    public static void main(String[] args) {
+        int a = 9;
+        double b = a;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}

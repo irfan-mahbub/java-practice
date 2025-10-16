4ab808e2b7c814d8e7af1799ecf635416f8738da
irfan-mahbub <talha.mahbub.irfan@gmail.com>
2025-10-16 18:05:59 +0600

    Narrowing Casting

---
+++ TC2.java
@@ -0,0 +1,11 @@
+// Narrowing Casting
+
+public class TC2 {
+    public static void main(String[] args) {
+        double b = 9.78d;
+        int a = (int) b;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}

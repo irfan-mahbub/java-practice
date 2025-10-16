fac04e0f36ed238066341ec54372c02e6d2fc434
irfan-mahbub <talha.mahbub.irfan@gmail.com>
2025-10-16 17:32:16 +0600

    Narrowing Casting

--- TC.java
+++ TC.java
@@ -1,11 +1,11 @@
-// Widening Casting
+// Narrowing Casting
 
 public class TC {
     public static void main(String[] args) {
-        int myInt = 9;
-        double myDouble = myInt; // Automatic casting: int to double
+        double myDouble = 9.78d;
+        int myInt = (int) myDouble; // Manual casting: double to int
 
+        System.out.println(myDouble); // Outputs 9.78
         System.out.println(myInt); // Outputs 9
-        System.out.println(myDouble); // Outputs 9.0
     }
 }
